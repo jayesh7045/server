16cfe8f266e51369e27f0f608e10e2f827b3e6ad
Jayeshw23092003 <jayesh.wadhwani_comp21@pccoer.in>
2025-02-13 10:29:13 +0530

    start the server

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 import express from "express"
+import dotenv from "dotenv"
+dotenv.config();
+
 const app = express();
+const port = process.env.PORT
+app.listen(port, ()=>{
+    console.log("App is listening on ", port)
+})
 app.get("/", (req, res)=>{
     res.send("Hello")
-} )
+})
